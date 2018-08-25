1c10d338f697923a59ddbd3289f855d27ba1db1f
armariya <arm.armariya@gmail.com>
2018-08-25 23:21:16 +0700

    maybe finish Bulbrary.sol

--- contracts/Bulbrary.sol
+++ contracts/Bulbrary.sol
@@ -15,12 +15,19 @@ contract Bulbrary is Ownable {
 
     BookERC721 private bookERC721Instance;
     mapping (uint => bool) private booksReserve; // bookId => boolean // true => reserve
+    mapping (uint => address) private booksReserveBy; // bookId => address
+    mapping (uint => address) private bookSellBy; // bookId => address
     mapping (address => uint256) private stakes; // address => sum of stake
     mapping (uint => uint32) private booksCanCancelAfter; // bookId => timestamp
     mapping (uint => string) private booksTrackingNumbers; // bookId => trackingNumber
 
     constructor(address _bookERC721Address) public {
         bookERC721Instance = BookERC721(_bookERC721Address); 
+    }
+
+    modifier bookMustReserve(uint _bookId) {
+        require(booksReserve[_bookId], "");
+        _;
     }
 
     // book
@@ -39,6 +46,8 @@ contract Bulbrary is Ownable {
     {
         uint bookId = bookERC721Instance.mint(_title, _hashImage, _author, _publisher, _price);
         booksReserve[bookId] = false;
+        booksReserveBy[bookId] = 0x0;
+        bookSellBy[bookId] = msg.sender;
     }
 
     // buyer
@@ -56,8 +65,8 @@ contract Bulbrary is Ownable {
     function stakeCoin(uint _bookId, uint256 buyerEth) private {
         stakes[msg.sender] += buyerEth;
         booksReserve[_bookId] = true;
+        booksReserveBy[_bookId] = msg.sender;
         booksCanCancelAfter[_bookId] = uint32(block.timestamp + (86400 * 4));
-
     }
 
     // seller call
@@ -68,16 +77,63 @@ contract Bulbrary is Ownable {
         addTrackingNumber(_bookId, _trackingNumber);
     }
 
-    // seller
-    function addTrackingNumber(uint _bookId, string _trackingNumber) private {
-        require(booksReserve[_bookId], "");
+    function getTrackingNumber(uint _bookId) public view returns (string) {
+        return booksTrackingNumbers[_bookId];
+    }
 
+    function addTrackingNumber(uint _bookId, string _trackingNumber) private bookMustReserve(_bookId) {
         booksTrackingNumbers[_bookId] = _trackingNumber;
     }
 
-    // owner approve automatic by server check after approximately 2 days
-    function approveReceivedBook(uint _bookId) public view onlyOwner {
+    // buyer call
+    function cancelBuyRequest(uint _bookId) public bookMustReserve(_bookId) {
+        require(uint32(block.timestamp) >= booksCanCancelAfter[_bookId], "");
+        clearReserveBook(_bookId);
+    }
 
+    // seller
+    function cancelOrder(uint _bookId) public bookMustReserve(_bookId) {
+        address buyer = booksReserveBy[_bookId];
+        (, , , , uint price) = bookERC721Instance.getBook(_bookId);
+        stakes[buyer] -= price;
+
+        clearReserveBook(_bookId);
+
+        buyer.transfer(price);
+    } 
+
+    // owner approve automatic by server check after approximately 2 days
+    function approveReceivedBook(uint _bookId) public onlyOwner {
+        require(_bookId >= 0, "bookId is index of array so that mean must greater than 0");
+        require(booksReserveBy[_bookId] != 0x0, "");
+
+        address seller = bookSellBy[_bookId];
+        address buyer = booksReserveBy[_bookId];
+        (, , , , uint price) = bookERC721Instance.getBook(_bookId);
+
+        subtractBuyerStake(buyer, price);
+        clearReserveBook(_bookId);
+        swapBookOwner(_bookId, buyer);
+
+        // send token to buyer
+        bookERC721Instance.transferFrom(address(this), buyer, _bookId);
+
+        // send ether to seller
+        seller.transfer(price);
+    }
+
+    function clearReserveBook(uint _bookId) private {
+        booksReserve[_bookId] = false;
+        booksReserveBy[_bookId] = 0x0;
+    }
+
+    function subtractBuyerStake(address _buyer, uint _bookPrice) private {
+        require(stakes[_buyer] > 0, "");
+        stakes[_buyer] -= _bookPrice;
+    }
+
+    function swapBookOwner(uint _bookId, address newOwner) public {
+        bookSellBy[_bookId] = newOwner;
     }
 
     // Fallback function
