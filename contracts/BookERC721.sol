789eefff9d173c72cddac34647d90a8b3153541a
armariya <arm.armariya@gmail.com>
2018-08-25 19:35:07 +0700

    add registerBook & buyBook functions

--- contracts/BookERC721.sol
+++ contracts/BookERC721.sol
@@ -12,17 +12,33 @@ contract BookERC721 is ERC721Token {
         string title;
         string hashImage;
         string author;
+        string publisher;
+        uint price;
     }
 
     Book[] public books;
 
-    function getBook(uint _bookId) public view returns (string, string, string) {
-        Book memory book = books[_bookId];
-        return (book.title, book.hashImage, book.author);
+    function getBookPrice(uint _bookId) external view returns (uint) {
+        require(books.length >= _bookId, "_bookId cannot greter than books.length array.");
+
+        (, , , , uint price) = this.getBook(_bookId);
+        return price;
     }
 
-    function mint(string _title, string _hashImage, string _author) public {
-        Book memory book = Book(_title, _hashImage, _author);
+    function isOwnerOfTheBook(address _userAddress, uint _bookId) external view returns (bool) {
+        require(_userAddress != address(0), "you forget to send seller address to us.");
+        require(_bookId >= 0, "bookId is index of array so that mean must greater than 0");
+
+        return _userAddress != this.ownerOf(_bookId);
+    }
+
+    function getBook(uint _bookId) external view returns (string, string, string, string, uint) {
+        Book memory book = books[_bookId];
+        return (book.title, book.hashImage, book.author, book.publisher, book.price);
+    }
+
+    function mint(string _title, string _hashImage, string _author, string _publisher, uint _price) public {
+        Book memory book = Book(_title, _hashImage, _author, _publisher, _price);
         uint bookId = books.push(book) - 1;
 
         _mint(msg.sender, bookId);
--- contracts/Bulbrary.sol
+++ contracts/Bulbrary.sol
@@ -1,11 +1,44 @@
 pragma solidity 0.4.24;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./BookERC721.sol";
 
 contract Bulbrary is Ownable {
 
-    constructor() public {
-       
+    struct Book {
+        string title;
+        string hashImage;
+        string author;
+        string publisher;
+        uint price;
+    }
+
+    BookERC721 private bookERC721Instance;
+
+    constructor(address _bookERC721Address) public {
+        bookERC721Instance = BookERC721(_bookERC721Address); 
+    }
+
+    function registerBook(
+        string _title, 
+        string _hashImage, 
+        string _author, 
+        string _publisher,
+        uint _price
+    ) 
+        external
+    {
+        bookERC721Instance.mint(_title, _hashImage, _author, _publisher, _price);
+    }
+
+    function buyBook(address _seller, uint _bookId) public payable {
+        require(_seller != address(0), "You forget to send seller address to us.");
+        require(_bookId >= 0, "bookId is index of array so that mean must greater than 0");
+        require(bookERC721Instance.isOwnerOfTheBook(_seller, _bookId), "This book is not owned by this seller.");
+        require(bookERC721Instance.getBookPrice(_bookId) <= msg.value, "Your money not enough for buy this book.");
+
+        // stakeCoin(msg.value);
+        // stakeBook(_bookId);
     }
 
     // Fallback function
