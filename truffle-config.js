10d0830da12fc734ab62017f44f52ff97e953480
Thanwa Jindarattana <thanwa@digitopolisstudio.com>
2018-08-25 14:58:59 +0700

    edit network file

--- truffle-config.js
+++ truffle-config.js
@@ -12,17 +12,21 @@
  *   },
  */
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  kovan: {
-    provider: function() { 
-      return new HDWalletProvider(
-      "rural wrap civil cash letter stable exhaust inject orchard divorce pyramid crop",
-      "https://kovan.infura.io/v3/1100994c16a34b70aae27b9818c3e205")
+  networks: {
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(
+          "kitchen board decrease material film plug gold agree shaft feature feed coffee",
+          "https://kovan.infura.io/v3/1100994c16a34b70aae27b9818c3e205")
+      },
+      network_id: '42',
+      gas: 4500000,
+      gasPrice: 10000000000,
     },
-    network_id: '42',
-    gas: 4500000,
-    gasPrice: 10000000000,
-  },
+  }
 };
