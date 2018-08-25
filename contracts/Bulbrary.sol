e93f133186d346b6b13898243b3859f17b108ddd
armariya <arm.armariya@gmail.com>
2018-08-25 21:48:52 +0700

    add stackCoin, stakeBook

--- contracts/Bulbrary.sol
+++ contracts/Bulbrary.sol
@@ -14,10 +14,19 @@ contract Bulbrary is Ownable {
     }
 
     BookERC721 private bookERC721Instance;
+    mapping (uint => bool) private booksReserve; // bookId => boolean // true => reserve
+    mapping (address => uint256) private stakes; // address => sum of stake
+    mapping (uint => uint32) private booksCanCancelAfter; // bookId => timestamp
+    mapping (uint => string) private booksTrackingNumbers; // bookId => trackingNumber
 
     constructor(address _bookERC721Address) public {
         bookERC721Instance = BookERC721(_bookERC721Address); 
     }
+
+    // book
+    // registerBook => A token
+    // seller => A token => buyer
+    // buyer (A token book)
 
     function registerBook(
         string _title, 
@@ -28,17 +37,47 @@ contract Bulbrary is Ownable {
     ) 
         external
     {
-        bookERC721Instance.mint(_title, _hashImage, _author, _publisher, _price);
+        uint bookId = bookERC721Instance.mint(_title, _hashImage, _author, _publisher, _price);
+        booksReserve[bookId] = false;
     }
+
+    // buyer
 
     function buyBook(address _seller, uint _bookId) public payable {
         require(_seller != address(0), "You forget to send seller address to us.");
         require(_bookId >= 0, "bookId is index of array so that mean must greater than 0");
         require(bookERC721Instance.isOwnerOfTheBook(_seller, _bookId), "This book is not owned by this seller.");
-        require(bookERC721Instance.getBookPrice(_bookId) <= msg.value, "Your money not enough for buy this book.");
+        require(bookERC721Instance.getBookPrice(_bookId) == msg.value, "Your money not enough for buy this book.");
+        require(!booksReserve[_bookId], "This book is already reserved by the other.");
 
-        // stakeCoin(msg.value);
-        // stakeBook(_bookId);
+        stakeCoin(_bookId, msg.value);
+    }
+
+    function stakeCoin(uint _bookId, uint256 buyerEth) private {
+        stakes[msg.sender] += buyerEth;
+        booksReserve[_bookId] = true;
+        booksCanCancelAfter[_bookId] = uint32(block.timestamp + (86400 * 4));
+
+    }
+
+    // seller call
+    function stakeBook(uint _bookId, string _trackingNumber) public {
+        // msg.sender call
+        address seller = msg.sender;
+        bookERC721Instance.transferFrom(seller, address(this), _bookId);
+        addTrackingNumber(_bookId, _trackingNumber);
+    }
+
+    // seller
+    function addTrackingNumber(uint _bookId, string _trackingNumber) private {
+        require(booksReserve[_bookId], "");
+
+        booksTrackingNumbers[_bookId] = _trackingNumber;
+    }
+
+    // owner approve automatic by server check after approximately 2 days
+    function approveReceivedBook(uint _bookId) public view onlyOwner {
+
     }
 
     // Fallback function
